471584c23c0d4e185be9c37e7c51ac7e8839a1a5
ngoctienle <ngoctienle.2399@gmail.com>
2023-05-28 23:59:04 +0700

    perf(joi): update a description and approve more errors from Express instead only one

--- src/shared/globals/decorators/joi-validation.decorator.ts
+++ src/shared/globals/decorators/joi-validation.decorator.ts
@@ -5,19 +5,31 @@ import { JoiRequestValidationError } from '@global/helpers/error-handler'
 
 type IJoiDecorator = (target: any, key: string, descriptor: PropertyDescriptor) => void
 
+/**
+ * Decorator function to perform Joi validation on an Express.js request body.
+ * @param schema - The Joi Schema to validate the request body against.
+ * @returns A method decorator that performs the validation.
+ */
 export function joiValidation(schema: ObjectSchema): IJoiDecorator {
-  return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
+  return (_target: unknown, _key: string, descriptor: PropertyDescriptor) => {
     const originalMethod = descriptor.value
 
-    descriptor.value = async function (...arg: any[]) {
-      const req: Request = arg[0]
-      const { error } = await Promise.resolve(schema.validate(req.body))
-      if (error?.details) {
-        throw new JoiRequestValidationError(error.details[0].message)
+    descriptor.value = function (...args: any[]) {
+      const req: Request = args[0]
+      // Check that the first argument is an Express request object and that it has a body.
+      if (!req || typeof req !== 'object' || !('body' in req)) {
+        throw new Error(`Expected the first argument to be a Request object with a body, but got ${typeof req}`)
       }
 
-      return originalMethod.apply(this, arg)
+      const { error } = schema.validate(req.body)
+      if (error?.details) {
+        const errors = error.details.map((err) => err.message)
+        throw new JoiRequestValidationError(errors.join('; '))
+      }
+
+      return originalMethod.apply(this, args)
     }
+
     return descriptor
   }
 }
